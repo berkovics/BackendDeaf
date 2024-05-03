5006c4d3e2f1ccc764ae27057dc2dfb8aea9f990
berkovics <berkovicsgellert@ktch.hu>
2024-05-03 19:30:57 +0200

    05.03

--- laravelDeaf.sql
+++ laravelDeaf.sql
@@ -1,15 +1,4 @@
--- Adatbázis
-
--- 1. lépés
 
 CREATE DATABASE laravel_deaf;
 
--- 2. lépés
--- Jogok root@localhost 
-
 GRANT ALL PRIVILEGES ON `laravel_deaf`.* TO 'root'@'localhost' WITH GRANT OPTION;
-
--- 3. lépés
--- BackendDeaf Parancsot:
-
--- php artisan migrate
